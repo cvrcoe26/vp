e17b3b6b578ea01dc757c6472f5bbeb4abff8fdb
cvrcoe26 <guru02721@gmail.com>
2025-10-15 17:50:24 +0530

    Create fibonacci.cs

---
+++ fibonacci.cs
@@ -0,0 +1,14 @@
+using System;
+class Program {
+    static void Main() {
+        Console.Write("Enter terms: ");
+        int n = int.Parse(Console.ReadLine());
+        int a=0, b=1, c;
+        for(int i = 0; i < n; i++) {
+            Console.Write(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
